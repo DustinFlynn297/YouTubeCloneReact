695fb14cb7fbbdc1a7bf1b7d97daa02ea17dcffb
Dustin Flynn <flynnd297@gmail.com>
2021-10-14 16:13:32 -0500

    Created a class component for App and added componentDidMount functions.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import axios from 'axios';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      video: []
+     }
+  }
+
+  componentDidMount(){
+    this.getVideos();
+  }
+
+  getVideos = async () => {
+    try {
+      let response = await axios.get('')
+    }
+  }
+
+  render() { 
+    return ( <div className="App">
       
-    </div>
-  );
+    </div> );
+  }
 }
+ 
 
 export default App;
